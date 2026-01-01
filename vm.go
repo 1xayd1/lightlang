56c3fddbfe5956b2bcee4dc8dc3b64684d13a795
1xayd <154957459+1xayd@users.noreply.github.com>
2026-01-01 17:19:33 +0500

    sync vm

--- vm.go
+++ vm.go
@@ -35,20 +35,21 @@ func NewVM() *VM {
 	}
 }
 
+type opFunc func(v *VM, f *Frame) error
+
 func toFloat64(val interface{}) float64 {
-	if v, ok := val.(float64); ok {
+	switch v := val.(type) {
+	case float64:
 		return v
-	}
-	if v, ok := val.(int); ok {
+	case int:
 		return float64(v)
-	}
-	if v, ok := val.(int64); ok {
+	case int64:
 		return float64(v)
-	}
-	if v, ok := val.(int32); ok {
+	case int32:
 		return float64(v)
+	default:
+		return 0.0
 	}
-	return 0.0
 }
 
 func (v *VM) loadBytecode(file string) error {
@@ -61,243 +62,354 @@ func (v *VM) loadBytecode(file string) error {
 	return nil
 }
 
-func (v *VM) Run(file string) error {
-	if file != "" {
-		if err := v.loadBytecode(file); err != nil {
-			return err
-		}
+func (v *VM) precompile() []opFunc {
+	ops := make([]opFunc, len(v.Instructions))
+	for i, inst := range v.Instructions {
+		ops[i] = v.makeOp(inst)
 	}
+	return ops
+}
 
-	v.CallStack = []Frame{{Instructions: v.Instructions, Ip: 0, Sp: 0}}
+func adaptOp(
+	genericHandler func(a, b interface{}) interface{},
+	floatHandler func(a, b float64) float64,
+) func(v *VM, f *Frame) error {
 
-	for len(v.CallStack) > 0 {
-		f := &v.CallStack[len(v.CallStack)-1]
+	var newfunc func(a, b interface{}) (interface{}, bool)
 
-		for f.Ip < len(f.Instructions) {
-			inst := f.Instructions[f.Ip]
-			f.Ip++
+	newfunc = func(a, b interface{}) (interface{}, bool) { return nil, false }
 
-			switch inst.Op {
-			case OpConstant:
-				idx := int(inst.Arg.(float64))
-				if idx >= 0 && idx < len(v.Constants) {
-					v.push(v.Constants[idx].Value)
+	return func(v *VM, f *Frame) error {
+		b := v.pop()
+		a := v.pop()
+
+		if res, ok := newfunc(a, b); ok {
+			v.push(res)
+			return nil
+		}
+		switch at := a.(type) {
+		case float64:
+			if bt, ok := b.(float64); ok {
+				if floatHandler != nil {
+					newfunc = func(x, y interface{}) (interface{}, bool) {
+						f1, ok1 := x.(float64)
+						f2, ok2 := y.(float64)
+						if ok1 && ok2 {
+							return floatHandler(f1, f2), true
+						}
+						return nil, false
+					}
+					v.push(floatHandler(at, bt))
+					return nil
 				}
-			case OpTable:
-				v.push(make(map[string]interface{}, 4))
-			case OpArray:
-				count := int(inst.Arg.(float64))
-				arr := make([]interface{}, count)
-				base := v.Sp - count
-				copy(arr, v.Stack[base:v.Sp])
-				v.Sp = base
-				v.push(arr)
-			case OpCmpEq, OpCmpNe:
-				b := v.pop()
-				a := v.pop()
-				eq := (a == b)
-				if inst.Op == OpCmpNe {
-					eq = !eq
+			}
+		}
+
+		v.push(genericHandler(a, b))
+		return nil
+	}
+}
+
+func (v *VM) makeOp(inst Instruction) opFunc {
+	switch inst.Op {
+	case OpConstant:
+		idx := int(inst.Arg.(float64))
+		val := v.Constants[idx].Value
+		return func(v *VM, f *Frame) error {
+			v.push(val)
+			return nil
+		}
+
+	case OpTable:
+		return func(v *VM, f *Frame) error {
+			v.push(make(map[string]interface{}, 4))
+			return nil
+		}
+
+	case OpArray:
+		count := int(inst.Arg.(float64))
+		return func(v *VM, f *Frame) error {
+			arr := make([]interface{}, count)
+			base := v.Sp - count
+			copy(arr, v.Stack[base:v.Sp])
+			v.Sp = base
+			v.push(arr)
+			return nil
+		}
+
+	case OpCmpEq:
+		return func(v *VM, f *Frame) error {
+			b := v.pop()
+			a := v.pop()
+			if a == b {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpCmpNe:
+		return func(v *VM, f *Frame) error {
+			b := v.pop()
+			a := v.pop()
+			if a != b {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpCmpLt:
+		return func(v *VM, f *Frame) error {
+			b := v.pop()
+			a := v.pop()
+			if af, ok1 := a.(float64); ok1 {
+				if bf, ok2 := b.(float64); ok2 {
+					if af < bf {
+						v.push(1.0)
+					} else {
+						v.push(0.0)
+					}
+					return nil
 				}
-				if eq {
-					v.push(1.0)
-				} else {
-					v.push(0.0)
-				}
-			case OpCmpLt:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				if a < b {
-					v.push(1.0)
-				} else {
-					v.push(0.0)
-				}
-			case OpCmpLte:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				if a <= b {
-					v.push(1.0)
-				} else {
-					v.push(0.0)
-				}
-			case OpCmpGt:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				if a > b {
-					v.push(1.0)
-				} else {
-					v.push(0.0)
-				}
-			case OpCmpGte:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				if a >= b {
-					v.push(1.0)
-				} else {
-					v.push(0.0)
-				}
-			case OpAdd:
-				b := v.pop()
-				a := v.pop()
-				switch av := a.(type) {
+			}
+			if toFloat64(a) < toFloat64(b) {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+	case OpCmpLte:
+		return func(v *VM, f *Frame) error {
+			b := toFloat64(v.pop())
+			a := toFloat64(v.pop())
+			if a <= b {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpCmpGt:
+		return func(v *VM, f *Frame) error {
+			b := toFloat64(v.pop())
+			a := toFloat64(v.pop())
+			if a > b {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpCmpGte:
+		return func(v *VM, f *Frame) error {
+			b := toFloat64(v.pop())
+			a := toFloat64(v.pop())
+			if a >= b {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpAdd:
+		genericAdd := func(a, b interface{}) interface{} {
+			switch av := a.(type) {
+			case float64:
+				switch bv := b.(type) {
 				case float64:
-					switch bv := b.(type) {
-					case float64:
-						v.push(av + bv)
-					case int:
-						v.push(av + float64(bv))
-					case string:
-						v.push(fmt.Sprintf("%v%v", av, bv))
-					}
+					return av + bv
 				case int:
-					switch bv := b.(type) {
-					case float64:
-						v.push(float64(av) + bv)
-					case int:
-						v.push(float64(av + bv))
-					case string:
-						v.push(fmt.Sprintf("%v%v", a, b))
-					case bool:
-						v.push(fmt.Sprintf("%v%v", a, b))
-					}
+					return av + float64(bv)
 				case string:
-					v.push(fmt.Sprintf("%v%v", a, b))
+					return fmt.Sprintf("%v%v", av, bv)
 				}
-			case OpSub:
-				b := v.pop()
-				a := v.pop()
-				bf := toFloat64(b)
-				af := toFloat64(a)
-				v.push(af - bf)
+			case int:
+				switch bv := b.(type) {
+				case float64:
+					return float64(av) + bv
+				case int:
+					return float64(av + bv)
+				case string:
+					return fmt.Sprintf("%v%v", a, b)
+				case bool:
+					return fmt.Sprintf("%v%v", a, b)
+				}
+			case string:
+				if bs, ok := b.(string); ok {
+					return av + bs
+				}
+				return fmt.Sprintf("%s%v", av, b)
+			}
+			return fmt.Sprintf("%v%v", a, b)
+		}
 
-			case OpMul:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				v.push(a * b)
+		return adaptOp(genericAdd, func(a, b float64) float64 {
+			return a + b
+		})
 
-			case OpDiv:
-				b := toFloat64(v.pop())
-				a := toFloat64(v.pop())
-				if b == 0 {
-					return fmt.Errorf("div by zero")
+	case OpSub:
+		genericSub := func(a, b interface{}) interface{} {
+			return toFloat64(a) - toFloat64(b)
+		}
+		return adaptOp(genericSub, func(a, b float64) float64 {
+			return a - b
+		})
+
+	case OpMul:
+		genericMul := func(a, b interface{}) interface{} {
+			return toFloat64(a) * toFloat64(b)
+		}
+		return adaptOp(genericMul, func(a, b float64) float64 {
+			return a * b
+		})
+
+	case OpDiv:
+		return func(v *VM, f *Frame) error {
+			b := v.pop()
+			a := v.pop()
+
+			if af, ok1 := a.(float64); ok1 {
+				if bf, ok2 := b.(float64); ok2 {
+					if bf == 0 {
+						return fmt.Errorf("div by zero")
+					}
+					v.push(af / bf)
+					return nil
 				}
-				v.push(a / b)
-			case OpNot:
-				val := v.pop()
-				isTrue := true
-				if val == nil || val == 0.0 || val == false || val == "" {
-					isTrue = false
-				}
-				if isTrue {
-					v.push(0.0)
+			}
+			bf := toFloat64(b)
+			if bf == 0 {
+				return fmt.Errorf("div by zero")
+			}
+			af := toFloat64(a)
+			v.push(af / bf)
+			return nil
+		}
+
+	case OpNot:
+		return func(v *VM, f *Frame) error {
+			val := v.pop()
+			if val == nil || val == 0.0 || val == false || val == "" {
+				v.push(1.0)
+			} else {
+				v.push(0.0)
+			}
+			return nil
+		}
+
+	case OpSetGlobal:
+		key := inst.Arg.(string)
+		return func(v *VM, f *Frame) error {
+			v.Globals[key] = v.pop()
+			return nil
+		}
+
+	case OpGetGlobal:
+		name := inst.Arg.(string)
+		return func(v *VM, f *Frame) error {
+			if val, ok := v.Globals[name]; ok {
+				v.push(val)
+			} else {
+				v.push(nil)
+			}
+			return nil
+		}
+
+	case OpSetLocal:
+		idx := int(inst.Arg.(float64))
+		return func(v *VM, f *Frame) error {
+			v.Stack[f.Sp+idx] = v.pop()
+			return nil
+		}
+
+	case OpGetLocal:
+		idx := int(inst.Arg.(float64))
+		return func(v *VM, f *Frame) error {
+			v.push(v.Stack[f.Sp+idx])
+			return nil
+		}
+
+	case OpGetIndex:
+		return func(v *VM, f *Frame) error {
+			index := v.pop()
+			table := v.pop()
+			switch t := table.(type) {
+			case []interface{}:
+				i := int(toFloat64(index))
+				if i >= 0 && i < len(t) {
+					v.push(t[i])
 				} else {
-					v.push(1.0)
+					v.push(nil)
 				}
-			case OpSetGlobal:
-				val := v.pop()
-				key := inst.Arg.(string)
-				v.Globals[key] = val
-			case OpGetGlobal:
-				name := inst.Arg.(string)
-				if val, ok := v.Globals[name]; ok {
+			case map[string]interface{}:
+				key := fmt.Sprintf("%v", index)
+				if val, ok := t[key]; ok {
 					v.push(val)
 				} else {
 					v.push(nil)
 				}
-			case OpSetLocal:
-				idx := int(inst.Arg.(float64))
-				val := v.pop()
-				v.Stack[f.Sp+idx] = val
-			case OpGetLocal:
-				idx := int(inst.Arg.(float64))
-				val := v.Stack[f.Sp+idx]
-				v.push(val)
-			case OpGetIndex:
-				index := v.pop()
-				table := v.pop()
-				switch t := table.(type) {
-				case []interface{}:
-					i := int(toFloat64(index))
-					if i >= 0 && i < len(t) {
-						v.push(t[i])
-					} else {
-						v.push(nil)
-					}
-				case map[string]interface{}:
-					key := fmt.Sprintf("%v", index)
-					if val, ok := t[key]; ok {
-						v.push(val)
-					} else {
-						v.push(nil)
-					}
+			default:
+				v.push(nil)
+			}
+			return nil
+		}
+
+	case OpSetIndex:
+		return func(v *VM, f *Frame) error {
+			val := v.pop()
+			index := v.pop()
+			table := v.pop()
+			switch t := table.(type) {
+			case []interface{}:
+				var i int
+				switch idx := index.(type) {
+				case float64:
+					i = int(idx)
+				case int:
+					i = idx
+				default:
+					i = int(toFloat64(index))
 				}
-			case OpSetIndex:
-				val := v.pop()
-				index := v.pop()
-				table := v.pop()
-				switch t := table.(type) {
-				case []interface{}:
-					var i int
-					switch idx := index.(type) {
-					case float64:
-						i = int(idx)
-					case int:
-						i = idx
-					default:
-						i = int(toFloat64(index))
-					}
-					if i >= 0 && i < len(t) {
-						t[i] = val
-					}
-					v.push(t)
-				case map[string]interface{}:
-					key := fmt.Sprintf("%v", index)
-					t[key] = val
-					v.push(t)
+				if i >= 0 && i < len(t) {
+					t[i] = val
 				}
-			case OpCall:
-				count := int(toFloat64(v.pop()))
-				target := inst.Arg.(string)
+				v.push(t)
+			case map[string]interface{}:
+				key := fmt.Sprintf("%v", index)
+				t[key] = val
+				v.push(t)
+			}
+			return nil
+		}
 
-				if fn, ok := builtins.Builtins[target]; ok {
-					args := make([]interface{}, count)
-					base := v.Sp - count
-					copy(args, v.Stack[base:v.Sp])
-					v.Sp = base
-
-					res, err := fn(args)
-					if err != nil {
-						return err
-					}
-
-					if res != nil {
-						v.push(res)
-					} else {
-						v.push(nil)
-					}
-					continue
+	case OpCall:
+		target := inst.Arg.(string)
+		return func(v *VM, f *Frame) error {
+			count := int(toFloat64(v.pop()))
+			if fn, ok := builtins.Builtins[target]; ok {
+				args := make([]interface{}, count)
+				base := v.Sp - count
+				copy(args, v.Stack[base:v.Sp])
+				v.Sp = base
+				res, err := fn(args)
+				if err != nil {
+					return err
 				}
-
-				if val, ok := v.Globals[target]; ok {
-					if fnMeta, ok := val.(map[string]interface{}); ok {
-						if t, ok := fnMeta["type"]; ok && t == "function" {
-							entry := int(fnMeta["entry"].(float64))
-							baseSp := v.Sp - count
-							v.CallStack = append(v.CallStack, Frame{
-								Instructions: v.Instructions,
-								Ip:           entry,
-								Sp:           baseSp,
-								ArgCount:     count,
-							})
-							continue
-						}
-					}
+				if res != nil {
+					v.push(res)
+				} else {
+					v.push(nil)
 				}
-				return fmt.Errorf("function '%s' not found", target)
-
-			case OpCallIndirect:
-				count := int(v.pop().(float64))
-				val := v.pop()
+				return nil
+			}
+			if val, ok := v.Globals[target]; ok {
 				if fnMeta, ok := val.(map[string]interface{}); ok {
 					if t, ok := fnMeta["type"]; ok && t == "function" {
 						entry := int(fnMeta["entry"].(float64))
@@ -308,64 +420,120 @@ func (v *VM) Run(file string) error {
 							Sp:           baseSp,
 							ArgCount:     count,
 						})
-						continue
+						return nil
 					}
 				}
-				return fmt.Errorf("cannot call non-function")
-
-			case OpReturn:
-				var retVal interface{}
-				if v.Sp > f.Sp {
-					retVal = v.pop()
-				} else {
-					retVal = nil
-				}
-				v.CallStack = v.CallStack[:len(v.CallStack)-1]
-
-				if len(v.CallStack) > 0 {
-					callerFrame := &v.CallStack[len(v.CallStack)-1]
-					v.Sp = callerFrame.Sp
-					v.push(retVal)
-				} else {
-					v.Sp = 0
-				}
-
-				f.Ip = len(f.Instructions)
-				continue
-			case OpMakeFunc:
-				idx := int(inst.Arg.(float64))
-				entry := v.Constants[idx].Value
-				fnObj := map[string]interface{}{
-					"type":  "function",
-					"entry": entry,
-				}
-				v.push(fnObj)
-			case OpJump:
-				target := int(toFloat64(inst.Arg))
-				f.Ip = target
-			case OpJumpIfFalse:
-				cond := v.pop()
-				isFalse := false
-				if cond == nil || cond == 0.0 || cond == false {
-					isFalse = true
-				}
-				if isFalse {
-					target := int(toFloat64(inst.Arg))
-					f.Ip = target
-				}
-			case OpPop:
-				if v.Sp > 0 {
-					v.Sp--
-				}
-			case OpHalt:
-				return nil
 			}
+			return fmt.Errorf("function '%s' not found", target)
 		}
 
-		if len(v.CallStack) == 1 {
+	case OpCallIndirect:
+		return func(v *VM, f *Frame) error {
+			count := int(v.pop().(float64))
+			val := v.pop()
+			if fnMeta, ok := val.(map[string]interface{}); ok {
+				if t, ok := fnMeta["type"]; ok && t == "function" {
+					entry := int(fnMeta["entry"].(float64))
+					baseSp := v.Sp - count
+					v.CallStack = append(v.CallStack, Frame{
+						Instructions: v.Instructions,
+						Ip:           entry,
+						Sp:           baseSp,
+						ArgCount:     count,
+					})
+					return nil
+				}
+			}
+			return fmt.Errorf("cannot call non-function")
+		}
+
+	case OpReturn:
+		return func(v *VM, f *Frame) error {
+			frameSp := f.Sp
+			var retVal interface{}
+			if v.Sp > frameSp {
+				retVal = v.pop()
+			} else {
+				retVal = nil
+			}
+			v.CallStack = v.CallStack[:len(v.CallStack)-1]
+			if len(v.CallStack) > 0 {
+				v.Sp = frameSp
+				v.push(retVal)
+			} else {
+				v.Sp = 0
+			}
 			return nil
 		}
-		v.CallStack = v.CallStack[:len(v.CallStack)-1]
+
+	case OpMakeFunc:
+		idx := int(inst.Arg.(float64))
+		entry := v.Constants[idx].Value
+		return func(v *VM, f *Frame) error {
+			fnObj := map[string]interface{}{
+				"type":  "function",
+				"entry": entry,
+			}
+			v.push(fnObj)
+			return nil
+		}
+
+	case OpJump:
+		target := int(toFloat64(inst.Arg))
+		return func(v *VM, f *Frame) error {
+			f.Ip = target
+			return nil
+		}
+
+	case OpJumpIfFalse:
+		target := int(toFloat64(inst.Arg))
+		return func(v *VM, f *Frame) error {
+			cond := v.pop()
+			if cond == nil || cond == 0.0 || cond == false || cond == "" {
+				f.Ip = target
+			}
+			return nil
+		}
+
+	case OpPop:
+		return func(v *VM, f *Frame) error {
+			if v.Sp > 0 {
+				v.Sp--
+			}
+			return nil
+		}
+
+	case OpHalt:
+		return func(v *VM, f *Frame) error { return fmt.Errorf("_HALT_") }
+	}
+
+	return func(v *VM, f *Frame) error { return nil }
+}
+
+func (v *VM) Run(file string) error {
+	if file != "" {
+		if err := v.loadBytecode(file); err != nil {
+			return err
+		}
+	}
+	compiledOps := v.precompile()
+	v.CallStack = []Frame{{Instructions: v.Instructions, Ip: 0, Sp: 0}}
+	for len(v.CallStack) > 0 {
+		f := &v.CallStack[len(v.CallStack)-1]
+		currentStackDepth := len(v.CallStack)
+		for f.Ip < len(compiledOps) {
+			op := compiledOps[f.Ip]
+			f.Ip++
+			if err := op(v, f); err != nil {
+				if err.Error() == "_HALT_" {
+					return nil
+				}
+				return err
+			}
+			if len(v.CallStack) != currentStackDepth {
+				break
+			}
+		}
 	}
 	return nil
 }
